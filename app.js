bc78174925562a04b7f21db4368403a2459ce904
ZodarSxK <athiphong1258@gmail.com>
2023-03-24 20:45:33 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,135 @@
+var express = require("express");
+
+var bodyParser = require("body-parser");
+
+var app = express();
+
+const db = require("./db.js");
+
+// เข้ารหัสส่งข้อมูล
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json());
+
+// start server
+
+app.listen(3000, () => {
+  console.log("Server running port 3000");
+});
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+// แสดงผล
+app.get("/", function (req, res) {
+  res.send("Hello Restful database API");
+});
+
+
+app.get("/testdb", function (req, res) {
+  res.send("Want");
+});
+// ดึงข้อมูล db
+
+// เรียกข้อมูลใน db มาแสดง
+
+app.get("/orderall", function (req, res) {
+  // ติดต่อ db
+
+  let sql = "SELECT order_no,product_name,product_source,order_num,cust_id,price FROM tb_order";
+
+  db.query(sql, (err, result, fields) => {
+    if (err) {
+      res.status(500).json({
+        status: 500,
+
+        message: "ไม่สามารถแสดงข้อมูลได้ :" + err.sqlMessage,
+      });
+    } else {
+      let data = { Data: result };
+
+      res.json(data);
+    }
+  });
+});
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+// ส่งค่าไปเพิ่อเพิ่มข้อมูลใน ฐานข้อมูล
+app.post('/insertorder', function (req, res) {
+    // รับค่าผ่าน form post
+    let data = {
+        cust_id: req.body.cust_id,
+        product_name: req.body.product_name,
+        order_num: req.body.order_num,
+        product_source: req.body.product_source,
+        price: req.body.price
+    }
+
+ // บันทึกข้อมูลลง db
+    let sql = 'INSERT INTO tb_order SET ?'
+    db.query(sql, data, (err, results, fields) => {
+        if (err) {
+            res.status(500).json({
+                'status': 500,
+                'message': 'ไม่สามารถเพิ่มข้อมูลได้ :' + err.sqlMessage
+            })
+        } else {
+            let result = {
+                'status' : 200,
+                'message' : 'เพิ่มข้อมูลเรียบร้อย'
+            }
+            res.json(result)
+        }
+    })
+})
+
+app.get('/deleteorder', function (req, res) {
+  // รับค่าผ่าน form post
+  let data = {
+      cust_id: req.body.cust_id,
+      product_name: req.body.product_name
+  }
+
+// บันทึกข้อมูลลง db
+  let sql = 'INSERT INTO tb_order SET ?'
+  db.query(sql, data, (err, results, fields) => {
+      if (err) {
+          res.status(500).json({
+              'status': 500,
+              'message': 'ไม่สามารถเพิ่มข้อมูลได้ :' + err.sqlMessage
+          })
+      } else {
+          let result = {
+              'status' : 200,
+              'message' : 'เพิ่มข้อมูลเรียบร้อย'
+          }
+          res.json(result)
+      }
+  })
+})
+
+
+//////////////////////////////////////////////////////////////////////////////////////////
+
+//select where
+
+app.get('/orderwherecust_id',function(req,res){
+    // รับค่าผ่าน form get
+    let cust_id = req.query.cust_id
+    // ติดต่อ db
+    let sql = 'SELECT order_no,product_name,order_num,product_source,price FROM tb_order WHERE cust_id=?'
+    db.query(sql, cust_id, (err, results, fields) => {
+        if (err) {
+            res.status(500).json({
+                'status': 500,
+                'message': 'ไม่พบข้อมูล :' + err.sqlMessage
+            });
+        } else {
+                let rs= {'Data': results};
+        res.json(rs);
+        }
+    })
+})
---
+++ db.js
@@ -0,0 +1,16 @@
+const mysql=require('mysql');
+   
+    const db = mysql.createConnection({
+        host : 'localhost',
+        user : 'root',
+        password : '',
+        database : 'db_futter1'
+    })
+    db.connect((err)=>{
+        if(err){
+           console.log('error connect db :'+err)
+        }else{
+           console.log('connect database Success!!')
+      }
+    })
+    module.exports = db
